186899c9c7d3354c296e0f66c9844c1f813c1b2f
Odunayo Olajumoke Okebunmi <okebunmiodunayo@gmail.com>
2019-04-15 22:52:25 +0100

    [feature #165346809] add loan calculator

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,8 +1,25 @@
-var slider = document.getElementById("amount-range");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+const amountRange = document.getElementById("amount-range");
+const amountValue = document.querySelector('.loan-amount__value');
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-  output.innerHTML = this.value;
+const daysRange = document.getElementById("days-range");
+const daysValue = document.querySelector('.loan-duration__value');
+
+const interestOutput = document.querySelector('.loan-interest__value');
+
+const interest = 0.05;
+// Display the default amountRange value
+amountValue.innerHTML = amountRange.value;
+daysValue.innerHTML = daysRange.value;
+
+interestOutput.innerHTML = (interest * amountRange.value);
+// Update the current amountRange value (each time you drag the amountRange handle)
+amountRange.oninput = function () {
+  amountValue.innerHTML = this.value;
+
+  interestOutput.innerHTML = (interest * amountRange.value);
+}
+
+daysRange.oninput = function () {
+  daysValue.innerHTML = this.value;
+  interestOutput.innerHTML = (interest * amountRange.value) + (85 * (daysRange.value - 1))
 }
