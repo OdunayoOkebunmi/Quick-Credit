14947cb85768f8c6f5afd2bd5a133b9611445fb8
Odunayo Olajumoke Okebunmi <okebunmiodunayo@gmail.com>
2019-05-18 22:55:27 +0100

    feature(users):persist data to db
    this persists users data to database
    
    [delivers]  #166071074

--- API/src/migrations/index.js
+++ API/src/migrations/index.js
@@ -1,22 +1,25 @@
+import debug from 'debug';
 import pool from './db';
 
 
+const log = debug('dev');
+
 const query = async (queryString) => {
   pool.on('connect', () => {
-    console.log('connected to the db');
+    log('connected to the db');
   });
   pool.query(queryString)
     .then((res) => {
-      console.log(res);
+      log(res);
       pool.end();
     })
     .catch((err) => {
-      console.log(err);
+      log(err);
       pool.end();
     });
 
   pool.on('remove', () => {
-    console.log('client removed');
+    log('client removed');
     process.exit(0);
   });
 };
