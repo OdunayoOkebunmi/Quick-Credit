7f861feb8099f91190e8ec616063e772002a9844
Odunayo Olajumoke Okebunmi <okebunmiodunayo@gmail.com>
2019-06-12 16:55:31 +0100

    feature(user login):fetch API
    this consumes the login endpoint using fetch API so that user can login
    
    [delivers 166636633]

--- UI/js/login.js
+++ UI/js/login.js
@@ -1,22 +1,17 @@
-const url = 'https://quick-credit-odunayo.herokuapp.com/api/v1/auth/signup';
-const signupForm = document.getElementById('sign-up-form');
-const firstName = document.getElementById('new-user-fname');
-const lastName = document.getElementById('new-user-lname');
-const email = document.getElementById('new-user-email');
-const address = document.getElementById('new-user-address');
-const password = document.getElementById('new-user-password');
+const url = 'https://quick-credit-odunayo.herokuapp.com/api/v1/auth/signin';
+const loginForm = document.getElementById('login-form');
+const email = document.getElementById('user-email');
+const password = document.getElementById('user-password');
+const popup = document.querySelector('.popup');
 
-signupForm.addEventListener('submit', (e) => {
+loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
   fetch(url, {
     method: 'POST',
     mode: 'cors',
     body: JSON.stringify({
-      firstName: firstName.value,
-      lastName: lastName.value,
       email: email.value,
       password: password.value,
-      address: address.value,
     }),
     headers: {
       'Content-Type': 'application/json',
@@ -25,25 +20,32 @@ signupForm.addEventListener('submit', (e) => {
     .then(res => res.json())
     .then(response => {
       if (response.error) {
+
         showError(response.error);
+        password.value = '';
       }
       if (response.data) {
         location.href = '#user-loan-popup';
         setTimeout(() => {
-          window.location = 'login.html'
-        }, 5000);
+          if (response.data.email.endsWith('quickcredit.com')) {
+            location.href = 'admin-loan-applications.html';
+          }
+          else {
+            location.href = 'user-dashboard.html'
+          }
+        }, 3000);
+
         localStorage.setItem('token', response.data.token);
         localStorage.setItem('userDetails', JSON.stringify(response.data));
         localStorage.setItem('loggedIn', true);
       }
-
     })
     .catch((error) => {
       const message = 'Error in connecting, Please check your internet connection and try again'
       showError(message);
-    });
+    })
 
-});
+})
 
 const showError = (error) => {
   const errorDiv = document.createElement('div');
